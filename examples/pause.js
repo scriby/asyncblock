3cbab32720038c12812ec8f711aaf36f71b519ca
Chris Scribner <scriby@mint.(none)>
2011-12-03 19:59:25 -0500

    Reworking arguments to be simpler and allow room for enhancements later without breaking interface

--- examples/pause.js
+++
@@ -1,8 +0,0 @@
-var greenlight = require('../greenlight');
-greenlight(function(pause, resume) {
-	console.log('starting timer');
-	setTimeout(resume, 2000); 
-	pause();
-	console.log('two seconds passed');
-});
-console.log('the main context does not pause');
--- greenlight.js
+++ greenlight.js
@@ -1,17 +1,15 @@
 require('fibers');
 
 module.exports = function(fn) {
-	var red, green;
-	// current status: false .. red, true .. green	
-	var light = false; 
+    var flow = {};
 
     var buffers = {};
     var lights = {};
 
     // the fiber
-	var fiber  = Fiber(function() {
+	var fiber = Fiber(function() {
 		try {
-			fn(red, green, parallel);
+			fn(flow);
 		} catch(e) {
 			// throw in next tick so the context matches again if yielded.
 			process.nextTick(function() {
@@ -22,8 +20,10 @@ module.exports = function(fn) {
 
     var parallelCount = 0;
     var parallelFinished = 0;
-    var parallel = function(key){
+    
+    flow.add = function(key){
         parallelCount++;
+        lights[key] = true;
 
         return function(){
             parallelFinished++;
@@ -32,12 +32,15 @@ module.exports = function(fn) {
 
             args.key = key;
 
-            if (lights[key]) {
+            if(key == null){
+                //If no key is set, we don't care about tracking the result
+                fiber.run(args);
+            } else if (lights[key]) {
                 // green called on green.
                 // an async functions might call its callback before red() was called.
                 // so buffer its answer for call of red.
                 if (buffers[key] != null) {
-                    throw new Error('greenlight: green called twice on green light');
+                    throw new Error('Key ' + key + ' is in use');
                 }
                 buffers[key] = args;
             } else {
@@ -47,21 +50,15 @@ module.exports = function(fn) {
             }
         };
     };
-
-	// in case green gets called before red, its arguments are buffered here.
-	var buffer = null;
-
-	red = function() {
-		if (!light) {
-			process.nextTick(function() {
-				throw new Error('greenlight: red called on red light');
-			});
-		}
-
+    
+	flow.wait = function() {
         var greenArgs = {};
 
         if(parallelCount > 0){
             while(parallelFinished < parallelCount){
+                //Reset lights every time through the loop such that new async callbacks can be added
+                lights = {};
+                
                 var ret = Fiber.yield();
                 greenArgs[ret.key] = ret;
             }
@@ -94,79 +91,10 @@ module.exports = function(fn) {
             lights = {};
 
             return returnValue;
-
         } else {
-            if (buffer) {
-                greenArgs = buffer;
-                buffer = null;
-            } else {
-                light = false;
-                greenArgs = Fiber.yield();
-            }
+            return;
         }
-
-		var asw;
-		var aswType = null;
-		if (arguments.length === 0) {
-			// default behaviour ('!', '<')
-			if (greenArgs[0]) throw greenArgs[0];
-			return greenArgs[1];
-		}
-		for (var ai = 0; ai < arguments.length; ai++) {
-			var a = arguments[ai];
-			if(typeof(a) !== 'string' && !a instanceof String) {
-				throw new Error('greenlight: invalid parameter to red "'+a+'"');
-			}
-			// ignore nulls, '', false etc
-			if (!a || !a[0]) continue;
-			switch(a[0]) {
-			case '!' : 
-				// error test
-				if (greenArgs[ai]) throw greenArgs[ai];
-				break;
-			case '<' : 
-				// return value
-				if (aswType) throw new Error('greenlight: multiple return types to red');
-				asw = greenArgs[ai];
-				aswType = 'argument'; 
-				break;
-			case '#' : 
-				// return array
-				if (aswType) throw new Error('greenlight: multiple return types to red');
-				asw = greenArgs;
-				aswType = 'array'; 
-				break;
-			case (/[A-z]/.test(a[0]) && a[0]) : 
-				// return a table
-				if (aswType && aswType !== 'table') {
-					throw new Error('greenlight: multiple return types to red');
-				} 
-				asw = asw || {};
-				aswType = 'table';
-				asw[a] = greenArgs[ai];
-				break;
-			default :
-				throw new Error('greenlight: unknown parameter to red: ' + a);
-			}
-		}
-		return asw;
 	};
 
-	green = function() {
-		if (light) {
-			// green called on green.
-			// an async functions might call its callback before red() was called.
-			// so buffer its answer for call of red.
-			if (buffer !== null) {
-				throw new Error('greenlight: green called twice on green light');
-			}
-			buffer = Array.prototype.slice.call(arguments);
-		} else {
-			light = true;
-			fiber.run(Array.prototype.slice.call(arguments));
-		}
-	};
-
-	light = true;
 	fiber.run();
 }
