22700f4a4799d3a7e6bc226edc11392893933ac3
Chris Scribner <scriby@gmail.com>
2014-04-02 12:25:21 -0400

    Ensure only one instance of asyncblock loads per process to avoid some weird interactions between multiple separate includes

--- asyncblock.js
+++ asyncblock.js
@@ -1,3 +1,9 @@
+//ensure we only have a single asyncblock / Fibers instance running within the process
+if(process.__asyncblock_included__){
+  module.exports = process.__asyncblock_included__;
+  return;
+}
+
 module.exports = require('./lib/asyncblock.js');
 
 require('./lib/flow.js');
